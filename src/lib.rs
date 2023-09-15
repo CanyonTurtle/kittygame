cff9bace2a62b110439d00a5302793db09db4f99
Cannon Tuttle <c>
2023-09-14 23:42:07 -0600

    add songs! :D

--- src/lib.rs
+++ src/lib.rs
@@ -170,29 +170,87 @@ fn update() {
 
     let [btns_pressed_this_frame, gamepads] = get_inputs_this_frame();
 
-    
+    struct Song {
+        scale: [u16; 8],
+        f1_pitchchange_timer: u8,
+        f2_pitchchange_timer: u8,
+        measure_length: u8,
+        f1_note_duration: u8,
+        f2_note_duration: u8,
+        time_signature: (u8, u8),
+    }
 
-    fn play_bgm(timer: u32) {
-        const D_MAJOR_FREQUENCIES: [u32; 8] = [293, 329, 369, 392, 440, 493, 554, 587];
+    let happy_cat = Song {
+        scale: [294, 330, 370, 392, 440, 494, 554, 587],
+        f1_pitchchange_timer: 5,
+        f2_pitchchange_timer: 3,
+        measure_length: 12,
+        f1_note_duration: 5,
+        f2_note_duration: 3,
+        time_signature: (3, 4),
+    };
 
-        let freq1: usize = (timer as usize / 5) % D_MAJOR_FREQUENCIES.len();
-        let freq2: usize = (timer as usize / 3) % D_MAJOR_FREQUENCIES.len();
+    let sneak_cat = Song {
+        scale: [196, 220, 247, 262, 294, 330, 370, 392],
+        f1_pitchchange_timer: 17,
+        f2_pitchchange_timer: 5,
+        measure_length: 30,
+        f1_note_duration: 10,
+        f2_note_duration: 20,
+        time_signature: (1, 3),
+    };
+
+    let mosh_cat = Song {
+        scale: [196, 220, 247, 262, 294, 330, 370, 392],
+        f1_pitchchange_timer: 1,
+        f2_pitchchange_timer: 2,
+        measure_length: 5,
+        f1_note_duration: 1,
+        f2_note_duration: 15,
+        time_signature: (2, 5),
+    };
+
+    let rando_cat = Song {
+        scale: [247, 277, 311, 330, 370, 415, 466, 494],
+        // scale: [196, 220, 247, 262, 294, 330, 370, 392],
+        f1_pitchchange_timer: 19,
+        f2_pitchchange_timer: 7,
+        measure_length: 4,
+        f1_note_duration: 1,
+        f2_note_duration: 26,
+        time_signature: (4, 4),
+    };
+
+    let explore_cat = Song {
+        scale: [196, 220, 247, 262, 294, 330, 370, 392],
+        // scale: [196, 220, 247, 262, 294, 330, 370, 392],
+        f1_pitchchange_timer: 7,
+        f2_pitchchange_timer: 11,
+        measure_length: 4,
+        f1_note_duration: 12,
+        f2_note_duration: 18,
+        time_signature: (12, 3),
+    };
+
+    fn play_bgm(timer: u32, song: &Song) {
+        
+
+        let freq1: usize = (timer as usize / song.f1_pitchchange_timer as usize) % song.scale.len();
+        let freq2: usize = (timer as usize / song.f2_pitchchange_timer as usize) % song.scale.len();
 
 
-        const TIMESIG_MULT: u32 = 12;
-
-        const TIME_SIGNATURE_NUMERATOR: u32 = 3*TIMESIG_MULT;
-        const TIME_SIGNATURE_DENOMINATOR: u32 = 4*TIMESIG_MULT;
-        if timer % TIME_SIGNATURE_NUMERATOR == 0 {
-            tone(D_MAJOR_FREQUENCIES[freq1], TIME_SIGNATURE_NUMERATOR, 20, TONE_PULSE1);
+        let time_signature_numerator: u32 = song.time_signature.0 as u32*song.measure_length as u32;
+        let time_signature_denominator: u32 = song.time_signature.1 as u32*song.measure_length as u32;
+        if timer % time_signature_numerator == 0 {
+            tone(song.scale[freq1] as u32, song.f1_note_duration as u32, 20, TONE_PULSE1);
         }
-        if timer % TIME_SIGNATURE_DENOMINATOR == 0 {
-            tone(D_MAJOR_FREQUENCIES[freq2], TIME_SIGNATURE_DENOMINATOR, 20, TONE_PULSE2);
+        if timer % time_signature_denominator == 0 && (freq2 as i32).abs_diff(freq1 as i32) > 1 {
+            tone(song.scale[freq2] as u32, song.f2_note_duration as u32, 20, TONE_PULSE2);
         }
     }
     
     game_state.timer += 1;
-    play_bgm(game_state.timer);
+    play_bgm(game_state.timer, &happy_cat);
 
     match &mut game_state.game_mode {
         GameMode::NormalPlay => {
